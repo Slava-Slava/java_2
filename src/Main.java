6aaf5ce79e6ee6ed42b78161aee1700aabcac5fa
Slava Vasiliev <sb-fantom@yandex.ru>
2023-02-28 16:43:44 +0300

    README.md

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 546;
+        int price = 48754;
         int mile = price / 20;
         System.out.println("Начисленные мили: " + mile);
 
