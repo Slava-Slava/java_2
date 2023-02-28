22ff67259752954d280bce075c843aa9abbcaacc
Slava Vasiliev <sb-fantom@yandex.ru>
2023-02-28 16:41:20 +0300

    README.md

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int price = 546;
+        int mile = price / 20;
+        System.out.println("Начисленные мили: " + mile);
+
     }
 }
